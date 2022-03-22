d386c7e2c14bd6c130c05c4b969e27bd8e9c2205
Brad <bradleyasu@gmail.com>
2022-03-21 20:53:02 -0400

    updating install script to include pip

--- setup/install.sh
+++ setup/install.sh
@@ -3,18 +3,18 @@
 echo "Base Configurations..."
 
 sudo apt update
-sudo apt install python3-setuptools git-core python3-dev
+sudo apt install python3-setuptools git-core python3-dev python3-pip
 
 echo "Installing Dependencies..."
 
-sudo apt install pkg-config libgl1-mesa-dev libgles2-mesa-dev \
+sudo apt -y install pkg-config libgl1-mesa-dev libgles2-mesa-dev \
    libgstreamer1.0-dev \
    gstreamer1.0-plugins-{bad,base,good,ugly} \
    gstreamer1.0-{omx,alsa} libmtdev-dev \
    xclip xsel libjpeg-dev
 
-sudo apt-get install libfreetype6-dev libgl1-mesa-dev libgles2-mesa-dev libdrm-dev libgbm-dev libudev-dev libasound2-dev liblzma-dev libjpeg-dev libtiff-dev libwebp-dev git build-essential
-sudo apt-get install gir1.2-ibus-1.0 libdbus-1-dev libegl1-mesa-dev libibus-1.0-5 libibus-1.0-dev libice-dev libsm-dev libsndio-dev libwayland-bin libwayland-dev libxi-dev libxinerama-dev libxkbcommon-dev libxrandr-dev libxss-dev libxt-dev libxv-dev x11proto-randr-dev x11proto-scrnsaver-dev x11proto-video-dev x11proto-xinerama-dev
+sudo apt-get -y install libfreetype6-dev libgl1-mesa-dev libgles2-mesa-dev libdrm-dev libgbm-dev libudev-dev libasound2-dev liblzma-dev libjpeg-dev libtiff-dev libwebp-dev git build-essential
+sudo apt-get -y install gir1.2-ibus-1.0 libdbus-1-dev libegl1-mesa-dev libibus-1.0-5 libibus-1.0-dev libice-dev libsm-dev libsndio-dev libwayland-bin libwayland-dev libxi-dev libxinerama-dev libxkbcommon-dev libxrandr-dev libxss-dev libxt-dev libxv-dev x11proto-randr-dev x11proto-scrnsaver-dev x11proto-video-dev x11proto-xinerama-dev
 
 # Install SDL2
 wget https://libsdl.org/release/SDL2-2.0.10.tar.gz
@@ -56,9 +56,12 @@ sudo make install
 popd
 
 
-sudo apt install mesa-common-dev libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev
+sudo apt -y install mesa-common-dev libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev
 
 echo "Installing Kivy..."
 python3 -m pip install kivy[base] kivy_examples
 
 echo "Setting up PiHome..."
+cd ..
+chmod 755 ./launch.sh
+./launch.sh
