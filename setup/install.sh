2b8347d3ed5badedd576ebd2d5cf04a0e84d082e
Brad <bradleyasu@gmail.com>
2022-03-22 11:10:08 -0400

    updating install script to point to pihome cdn

--- setup/install.sh
+++ setup/install.sh
@@ -2,8 +2,8 @@
 
 echo "Base Configurations..."
 
-sudo apt update
-sudo apt install python3-setuptools git-core python3-dev python3-pip
+sudo apt -y update
+sudo apt -y install python3-setuptools git-core python3-dev python3-pip
 
 echo "Installing Dependencies..."
 
@@ -17,7 +17,7 @@ sudo apt-get -y install libfreetype6-dev libgl1-mesa-dev libgles2-mesa-dev libdr
 sudo apt-get -y install gir1.2-ibus-1.0 libdbus-1-dev libegl1-mesa-dev libibus-1.0-5 libibus-1.0-dev libice-dev libsm-dev libsndio-dev libwayland-bin libwayland-dev libxi-dev libxinerama-dev libxkbcommon-dev libxrandr-dev libxss-dev libxt-dev libxv-dev x11proto-randr-dev x11proto-scrnsaver-dev x11proto-video-dev x11proto-xinerama-dev
 
 # Install SDL2
-wget https://libsdl.org/release/SDL2-2.0.10.tar.gz
+wget https://cdn.pihome.io/bin/SDL2-2.0.10.tar.gz
 tar -zxvf SDL2-2.0.10.tar.gz
 pushd SDL2-2.0.10
 ./configure --enable-video-kmsdrm --disable-video-opengl --disable-video-x11 --disable-video-rpi
@@ -27,7 +27,7 @@ popd
 
 # Install SDL2_image:
 
-wget https://libsdl.org/projects/SDL_image/release/SDL2_image-2.0.5.tar.gz
+wget https://cdn.pihome.io/bin/SDL2_image-2.0.5.tar.gz
 tar -zxvf SDL2_image-2.0.5.tar.gz
 pushd SDL2_image-2.0.5
 ./configure
@@ -37,7 +37,7 @@ popd
 
 # Install SDL2_mixer:
 
-wget https://libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4.tar.gz
+wget https://cdn.pihome.io/bin/SDL2_mixer-2.0.4.tar.gz
 tar -zxvf SDL2_mixer-2.0.4.tar.gz
 pushd SDL2_mixer-2.0.4
 ./configure
@@ -47,7 +47,7 @@ popd
 
 # Install SDL2_ttf:
 
-wget https://libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.15.tar.gz
+wget https://cdn.pihome.io/bin/SDL2_ttf-2.0.15.tar.gz
 tar -zxvf SDL2_ttf-2.0.15.tar.gz
 pushd SDL2_ttf-2.0.15
 ./configure
@@ -57,6 +57,7 @@ popd
 
 
 sudo apt -y install mesa-common-dev libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev
+sudo apt -y install 
 
 echo "Installing Kivy..."
 python3 -m pip install kivy[base] kivy_examples
