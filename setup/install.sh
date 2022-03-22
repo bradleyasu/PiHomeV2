37d46948237321cd77d22e7ca41d568e64ff5f19
Brad <bradleyasu@gmail.com>
2022-03-22 18:01:31 -0400

    updating install script

--- setup/install.sh
+++ setup/install.sh
@@ -2,22 +2,49 @@
 
 # Install Script: sudo curl -sSL https://pihome.io/install | bash
 
-echo "Base Configurations..."
 
-sudo apt -y update
-sudo apt -y install python3-setuptools git-core python3-dev python3-pip
+# Colors
 
-echo "Installing Dependencies..."
+PINK=`tput setaf 200`
+ENDCOLOR=`tput sgr0`
+
+# Vars
+PROFILE=~/.bashrc
+PIHOME=/usr/local/PiHome
+LOG=$PIHOME/install.log
+
+echo $PINK
+echo "______ _ _   _                      ";
+echo "| ___ (_) | | |                     ";
+echo "| |_/ /_| |_| | ___  _ __ ___   ___ ";
+echo "|  __/| |  _  |/ _ \| '_ V _  \ / _ \\";
+echo "| |   | | | | | (_) | | | | | |  __/";
+echo "\_|   |_\_| |_/\___/|_| |_| |_|\___|";
+echo $ENDCOLOR
+echo "------------------------------------";
+echo ""
+
+echo "Setting up..."
+mkdir $PIHOME
+cd $PIHOME
+
+
+echo "Preparing system..."
+sudo apt -y update  >> $LOG
+
+echo "Installing Dependencies, this may take a few minutes..."
+sudo apt -y install python3-setuptools git-core python3-dev python3-pip >> $LOG
 
 sudo apt -y install pkg-config libgl1-mesa-dev libgles2-mesa-dev \
    libgstreamer1.0-dev \
    gstreamer1.0-plugins-{bad,base,good,ugly} \
    gstreamer1.0-{omx,alsa} libmtdev-dev \
-   xclip xsel libjpeg-dev
+   xclip xsel libjpeg-dev >> $LOG
 
-sudo apt-get -y install libfreetype6-dev libgl1-mesa-dev libgles2-mesa-dev libdrm-dev libgbm-dev libudev-dev libasound2-dev liblzma-dev libjpeg-dev libtiff-dev libwebp-dev git build-essential
-sudo apt-get -y install gir1.2-ibus-1.0 libdbus-1-dev libegl1-mesa-dev libibus-1.0-5 libibus-1.0-dev libice-dev libsm-dev libsndio-dev libwayland-bin libwayland-dev libxi-dev libxinerama-dev libxkbcommon-dev libxrandr-dev libxss-dev libxt-dev libxv-dev x11proto-randr-dev x11proto-scrnsaver-dev x11proto-video-dev x11proto-xinerama-dev
+sudo apt-get -y install libfreetype6-dev libgl1-mesa-dev libgles2-mesa-dev libdrm-dev libgbm-dev libudev-dev libasound2-dev liblzma-dev libjpeg-dev libtiff-dev libwebp-dev git build-essential >> $LOG
+sudo apt-get -y install gir1.2-ibus-1.0 libdbus-1-dev libegl1-mesa-dev libibus-1.0-5 libibus-1.0-dev libice-dev libsm-dev libsndio-dev libwayland-bin libwayland-dev libxi-dev libxinerama-dev libxkbcommon-dev libxrandr-dev libxss-dev libxt-dev libxv-dev x11proto-randr-dev x11proto-scrnsaver-dev x11proto-video-dev x11proto-xinerama-dev >> $LOG
 
+echo "Setting up Simple DirectMedia Layer, please wait..."
 # Install SDL2
 wget https://cdn.pihome.io/bin/SDL2-2.0.10.tar.gz
 tar -zxvf SDL2-2.0.10.tar.gz
@@ -58,13 +85,25 @@ sudo make install
 popd
 
 
-sudo apt -y install mesa-common-dev libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev
-sudo apt -y install libmtdev1
+sudo apt -y install mesa-common-dev libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev >> $LOG
+sudo apt -y install libmtdev1 >> $LOG
 
 echo "Installing Kivy..."
-python3 -m pip install kivy[base] kivy_examples
+python3 -m pip install kivy[base] >> $LOG
 
-echo "Setting up PiHome..."
+echo "Setting up environment..."
+
+echo "" >> $PROFILE
+echo alias pihome="cd ${PIHOME}/ && ./launch.sh" >> $PROFILE
+echo "" >> $PROFILE
+
+clear
+
+echo $PINK
+echo ""
+echo "Preparing to launch PiHome..."
+echo ""
+echo $ENDCOLOR
 cd ..
 chmod 755 ./launch.sh
 ./launch.sh
