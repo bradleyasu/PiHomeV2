92e42821c2e496f583bef7e895d21a4d35650b0a
Brad <bradleyasu@gmail.com>
2022-07-27 14:35:05 -0400

    cleanup network image

---
+++ components/Image/networkimage.py
@@ -0,0 +1,27 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.behaviors import ButtonBehavior 
+from kivy.uix.image import AsyncImage  
+from kivy.uix.label import Label
+from theme.theme import Theme
+from kivy.properties import ColorProperty, NumericProperty, StringProperty
+from kivy.animation import Animation
+from kivy.clock import Clock
+from kivy.uix.widget import Widget
+from kivy.metrics import dp
+
+Builder.load_file("./components/Image/networkimage.kv")
+
+class NetworkImage(Widget):
+    theme = Theme()
+
+    color = ColorProperty(theme.get_color(theme.BACKGROUND_PRIMARY))
+    def __init__(self, url = "", size=(dp(50), dp(50)), pos=(dp(10), dp(10)), **kwargs):
+        super(NetworkImage, self).__init__(**kwargs)
+        self.image = AsyncImage(source=url, pos=pos, size=size)
+        self.add_widget(self.image)
+        self.size = size
+        self.pos = pos
+
+
+
--- screens/Bus/bus.py
+++ screens/Bus/bus.py
@@ -4,15 +4,17 @@ from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import Screen
 from kivy.uix.label import Label
+from kivy.uix.image import AsyncImage
 from kivy.metrics import dp
 from kivy.properties import ColorProperty, StringProperty,ObjectProperty
 
 from components.Button.circlebutton import CircleButton
 from components.Button.simplebutton import SimpleButton
+from components.Image.networkimage import NetworkImage
 from theme.color import Color
 from theme.theme import Theme
 from kivy.factory import Factory
-from util.helpers import get_config, get_poller
+from util.helpers import get_app, get_config, get_poller, goto_screen
 from util.tools import hex
 
 Builder.load_file("./screens/Bus/bus.kv")
@@ -26,8 +28,9 @@ class BusScreen(Screen):
         super(BusScreen, self).__init__(**kwargs)
         api = get_config().get('bus', 'bus_eta_url', '')
         key = get_config().get('bus', 'bus_eta_key', '')
+        self.logo= get_config().get('bus', 'logo', '')
 
-        get_poller().register_api(api, key, 60, lambda json: print(json))
+        # get_poller().register_api(api, key, 60, lambda json: print(json))
 
         self.color = self.theme.get_color(self.theme.BACKGROUND_PRIMARY)
         self.slime = Color.DARK_INDIGO_500
@@ -36,15 +39,16 @@ class BusScreen(Screen):
     def build(self):
         layout = FloatLayout()
 
-        button = SimpleButton(text='Go Back Home', size=(dp(200), dp(50)), pos=(dp(10), dp(10)))
-        button.background_color = Color.CHARTREUSE_400
-        button.foreground_color = Color.GRAY_50
-        button.bind(on_release=lambda _: self.open_home())
-        layout.add_widget(button)
+
+
+        homeBtn = CircleButton(text='→', size=(dp(50), dp(50)), pos=(dp(get_app().width - 70), dp(get_app().height - 70)))
+        homeBtn.bind(on_release=lambda _: goto_screen('home'))
+        layout.add_widget(homeBtn)
+
+        self.logo = NetworkImage(url=self.logo, size=(dp(216), dp(112)), pos=(dp(get_app().width - 200), dp(0)))
+        layout.add_widget(self.logo)
 
         self.add_widget(layout)
 
-    def open_home(self):
-        self.manager.current = 'home'
 
 
