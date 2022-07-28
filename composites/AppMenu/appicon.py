ffb8b93b124d2bb8c084f1fcea7a6d3a9cea91c4
Brad <bradleyasu@gmail.com>
2022-07-28 13:52:34 -0400

    unused imports

--- composites/AppMenu/appicon.py
+++ composites/AppMenu/appicon.py
@@ -1,25 +1,7 @@
-from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.behaviors import ButtonBehavior 
-from kivy.uix.image import Image  
-from kivy.uix.label import Label
-from numpy import spacing
-from components.Button.circlebutton import CircleButton
-from components.Button.simplebutton import SimpleButton
-from components.Image.networkimage import NetworkImage
-from theme.color import Color
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.gridlayout import GridLayout
-from theme.theme import Theme
-from kivy.properties import ColorProperty, NumericProperty, StringProperty
-from kivy.uix.scrollview import ScrollView
-from kivy.animation import Animation
-from kivy.clock import Clock
+from kivy.properties import ColorProperty, StringProperty
 from kivy.metrics import dp
-from util.helpers import get_app
-from util.tools import hex
 from kivy.uix.widget import Widget
-from kivy.uix.button import Button
 
 Builder.load_file("./composites/AppMenu/AppIcon.kv")
 
