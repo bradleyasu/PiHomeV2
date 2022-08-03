45a3036901939894afd37416de995a478f5b8ccb
Brad <bradleyasu@gmail.com>
2022-08-02 20:46:10 -0400

    add brightness thing to devtools

--- screens/DevTools/devtools.py
+++ screens/DevTools/devtools.py
@@ -22,12 +22,14 @@ from util.helpers import appmenu_open, get_app, goto_screen
 from util.tools import hex
 from kivy.clock import Clock
 from kivy.animation import Animation
+from kivy.uix.slider import Slider
 
 Builder.load_file("./screens/DevTools/devtools.kv")
 
 class DevTools(PiHomeScreen):
     local_ip = StringProperty("0.0.0.0")
     theme = Theme()
+    slider = None
     def __init__(self, **kwargs):
         super(DevTools, self).__init__(**kwargs)
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -38,7 +40,7 @@ class DevTools(PiHomeScreen):
 
     
     def build(self):
-        layout = FloatLayout()
+        layout = FloatLayout(size=(dp(480), dp(600)), size_hint=(None, None))
 
         button = CircleButton(text='X', size=(dp(50), dp(50)), pos=(dp(self.width - 70), dp(self.height - 70)))
         button.stroke_color = self.theme.get_color(self.theme.ALERT_DANGER)
@@ -47,11 +49,19 @@ class DevTools(PiHomeScreen):
         button.bind(on_release=lambda _: self.trigger_update())
         layout.add_widget(button)
 
+
+        self.slider = Slider(on_touch_move=lambda x,y: self.set_brightness(self.slider.value), orientation='horizontal', min=20, max = 255, pos=(dp(self.width - 10), dp(self.height - 100)), size=(dp(40), dp(200)), step=1)
+        layout.add_widget(self.slider)
+        
         self.add_widget(layout)
 
     
     def trigger_update(self):
         subprocess.call(['sh', './update_and_restart.sh'])
+
+    def set_brightness(self, level):
+        # echo 32 > /sys/class/backlight/rpi_backlight/brightness
+        subprocess.call(['echo', level, '>', '/sys/class/backlight/rpi_backlight/brightness'])
 
 
   
