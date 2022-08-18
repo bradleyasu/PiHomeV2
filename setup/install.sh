1cf0a92b7440b517b433c20483a1b3497bb1a8c6
Brad <bradleyasu@gmail.com>
2022-08-18 08:42:19 -0400

    update script to update mpv after install

--- setup/install.sh
+++ setup/install.sh
@@ -118,6 +118,15 @@ python3 -m pip install qrcode[pil] >> $LOG
 python3 -m pip install mplayer.py >> $LOG
 python3 -m pip install python-mpv >> $LOG
 
+# Update MPV
+
+curl https://non-gnu.uvt.nl/debian/uvt_key.gpg --output uvt_key.gpg
+sudo mv uvt_key.gpg /etc/apt/trusted.gpg.d
+sudo apt install apt-transport-https
+sudo sh -c 'echo "deb https://non-gnu.uvt.nl/debian $(lsb_release -sc) uvt" >> /etc/apt/sources.list.d/non-gnu-uvt.list'
+sudo apt update
+sudo apt install -t "o=UvT" mpv
+
 echo "Installing Kivy..."
 python3 -m pip install kivy[base] >> $LOG
 
