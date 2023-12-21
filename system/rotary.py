871be1c123c1501eb792412443d4613e0fe482b0
Brad <bradleyasu@gmail.com>
2023-12-21 11:23:16 -0500

    tweak

--- system/rotary.py
+++ system/rotary.py
@@ -41,7 +41,7 @@ class RotaryEncoder():
             GPIO.setup(self.a_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
             GPIO.setup(self.b_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
             GPIO.setup(self.button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            GPIO.add_event_detect(self.button_pin, GPIO.BOTH, callback=self.on_press, bouncetime=300)
+            GPIO.add_event_detect(self.button_pin, GPIO.BOTH, callback=self.on_press, bouncetime=100)
             GPIO.add_event_detect(self.a_pin, GPIO.BOTH, callback=self.update)
             GPIO.add_event_detect(self.b_pin, GPIO.BOTH, callback=self.update)
             self.last_button_state = GPIO.input(self.button_pin)
