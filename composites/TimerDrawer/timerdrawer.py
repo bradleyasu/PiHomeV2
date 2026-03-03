fc1aa036bac67a1fbe74172e37ca8b18b90cc679
Brad <bradleyasu@gmail.com>
2026-03-03 18:34:21 -0500

    test

--- composites/TimerDrawer/timerdrawer.py
+++ composites/TimerDrawer/timerdrawer.py
@@ -89,7 +89,7 @@ class TimerDrawer(BoxLayout):
     def add_timer(self, timer):
         self._ensure_width()
         tw = PiHomeTimer(timer=timer)
-        # self.timer_widgets.append(tw)
+        self.timer_widgets.append(tw)
         # if 'tray' in self.ids:
         #     self.ids.tray.add_widget(tw)
         # timer.add_listener(
